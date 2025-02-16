9d95280b12c7f8c7c6bb958393dd3f6969a6b7c5
plexsoup <plexsoup@gmail.com>
2025-02-16 11:18:15 -0800

    placeholder stuff

---
+++ brackeys2025-game/Entities/factory_products/factory_product_widget.gd
@@ -0,0 +1,8 @@
+## This is the item on the conveyor belt.
+# could be raw material, upgraded material, parts, products, whatever
+# factories use these as inputs to convert them to other product widgets
+# customers want them
+# they can go in inventory or storage chests
+# they can pile up on assembly lines
+
+extends Node2D
