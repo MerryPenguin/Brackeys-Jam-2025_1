b03b2d580f86d1d394b406171549a34383e9bb92
plexsoup <plexsoup@gmail.com>
2025-02-15 22:33:46 -0800

    shorter animation on main menu

--- brackeys2025-game/GUI/maaack_template/scenes/opening/opening.gd
+++ brackeys2025-game/GUI/maaack_template/scenes/opening/opening.gd
@@ -3,5 +3,13 @@ extends "res://addons/maaacks_game_template/base/scenes/opening/opening.gd"
 func _ready():
 	super()
 	$BrainstormingPanel.queue_free()
-	
-	
+	if OS.is_debug_build():
+		#list_all_classes()
+		pass
+
+func list_all_classes():
+	print("Registered Classes")
+	var classes = ProjectSettings.get_global_class_list()
+	for item in classes:
+		print(item.class)
+		
