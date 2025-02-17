b194407e69ec83932ae86a67dc0d205aa0a2e56f
plexsoup <plexsoup@gmail.com>
2025-02-17 15:45:35 -0800

    set keycodes on button bar

--- brackeys2025-game/GUI/Widgets/button_bar.gd
+++ brackeys2025-game/GUI/Widgets/button_bar.gd
@@ -6,16 +6,24 @@
 extends GridContainer
 
 func _ready():
+	var button_num = 0
 	for item in get_children():
 		if item is Button:
-			setup_button(item)
+			setup_button(item, button_num)
+			button_num += 1
 			
-func setup_button(button : Button):
+func setup_button(button : Button, button_num: int):
+	
 	button.icon_alignment = HORIZONTAL_ALIGNMENT_CENTER
 	button.vertical_icon_alignment = VERTICAL_ALIGNMENT_CENTER
 	button.expand_icon = true
 	button.pressed.connect(_on_button_pressed.bind(button))
-
+	button.shortcut = Shortcut.new()
+	var event_key = InputEventKey.new()
+	var keycodes = [ KEY_0, KEY_1, KEY_2, KEY_3, KEY_4, KEY_5, KEY_6 ]
+	event_key.keycode = keycodes[button_num]
+	button.shortcut.events.push_back(event_key)
+	
 
 func _on_button_pressed(button : Button):
 	if "Harvester" in button.name:
